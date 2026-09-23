6556f60cd7f9b0a941a8a9ab18d8f6de83f436da
undarez <fortuna77320@hotmail.fr>
2026-09-23 21:24:19 +0200

    P2: expose autonomous supervisor API

--- src/lib/lia/agents/index.ts
+++ src/lib/lia/agents/index.ts
@@ -3,3 +3,4 @@ export * from "./registry";
 export * from "./runtime";
 export * from "./executor";
 export * from "./safe-skills";
+\nexport * from "./supervisor";\nexport * from "./supervisor-types";\n
